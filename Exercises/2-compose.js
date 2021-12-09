65b8e000ff772283561c1a1c58fc8872489153ed
m1guelten <footballMiguelTen@gmail.com>
2021-12-09 21:09:17 +0200

    task 1-2

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const compose = (...fns) => x => null;
-
+const compose = (...fns) => {
+    const handlers = {};
+    const comp = x => {
+      if (fns.length === 0) {
+        return x;
+      }
+      let res = x;
+      try {
+        for (let i = (fns.length -1); i >= 0; i--) {
+          res = fns[i](res);
+        }
+        return res;
+      } 
+      catch (error) {
+      handlers.error=error;
+      }    
+    };
+    comp.on = (name) => {
+      if (name === 'error'){return handlers.error};
+    }
+    return comp;
+  };
+  
 module.exports = { compose };
